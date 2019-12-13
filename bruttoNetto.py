ce4bad3c48800fd10cda387fe9a66fb13b548fda
Dominik <dombastik@o2.pl>
2019-12-13 16:41:29 +0100

    reading earnings as script arguments

--- bruttoNetto.py
+++ bruttoNetto.py
@@ -1,18 +1,20 @@
-import scrapy
+import scrapy, sys
 from scrapy.crawler import CrawlerProcess
+from queue import Queue
 
 
 class EarningsSpider(scrapy.Spider):
 
-    def __init__(self, name=None, **kwargs):
+    def __init__(self, earnings, queue, name=None, **kwargs):
         super().__init__(name, **kwargs)
-        earnings = str(kwargs.get('earnings', '5000'))
+        self.queue = queue
+        self.earnings = str(earnings)
         self.post_data = {
-            'sedlak_calculator[earnings]': earnings
+            'sedlak_calculator[earnings]': self.earnings
         }
         for month_number in range(12):
             post_data_key = 'sedlak_calculator[monthlyEarnings][{}]'.format(month_number)
-            self.post_data[post_data_key] = earnings
+            self.post_data[post_data_key] = self.earnings
 
     name = 'wynagrodzenia.pl'
     start_urls = ['https://wynagrodzenia.pl/kalkulator-wynagrodzen/']
@@ -31,12 +33,41 @@ class EarningsSpider(scrapy.Spider):
         salary_div = response.css('div.count-salary')
         spans_in_salary_div = salary_div.css('span::text')
         salary = spans_in_salary_div.get()
-        return {'salary': salary}
+        # extract only digits from salary and cut off the cents
+        salary_no_cents = ''.join(list(filter(str.isdigit, salary)))[:-2]
+        self.queue.put([self.earnings, salary_no_cents])
 
 
-process = CrawlerProcess(settings={
-    'FEED_FORMAT': 'json',
-    'FEED_URI': 'stdout:'
-})
-process.crawl(EarningsSpider, earnings='3500')
-process.start()
+class EarningsCalculator:
+
+    def __init__(self):
+        self.earnings_list = list()
+
+    def add_earnings(self, earnings):
+        self.earnings_list.append(earnings)
+
+    def get_salary(self):
+        earnings_count = len(self.earnings_list)
+        assert earnings_count > 0, 'no earnings to convert'
+        queue = Queue(maxsize=earnings_count)
+        process = CrawlerProcess(settings={
+            'FEED_FORMAT': 'json',
+            'FEED_URI': 'items.json'
+        })
+        for earning_to_convert in self.earnings_list:
+            process.crawl(EarningsSpider, earnings=earning_to_convert, queue=queue)
+        process.start()
+
+        waiting_for_results_count = earnings_count
+        while (waiting_for_results_count > 0):
+            result = queue.get(block=True, timeout=10)
+            waiting_for_results_count -= 1
+            yield result
+
+
+earnings_calculator = EarningsCalculator()
+arguments_count = len(sys.argv) - 1
+for argument in sys.argv[1:]:
+    earnings_calculator.add_earnings(argument)
+for result in earnings_calculator.get_salary():
+    print(result)
